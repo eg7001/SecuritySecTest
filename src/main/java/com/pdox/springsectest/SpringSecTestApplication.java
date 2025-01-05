378f804f89644539cc4542f5bac0a4e7b2a554e8
eg7001 <110489363+eg7001@users.noreply.github.com>
2025-01-05 02:51:01 +0100

    actual first commit

---
+++ src/main/java/com/pdox/springsectest/SpringSecTestApplication.java
@@ -0,0 +1,13 @@
+package com.pdox.springsectest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pdox/springsectest/SpringSecTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdox.springsectest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
