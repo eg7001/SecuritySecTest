6ef3056b1b5a0c60852c08325ee4877e554fd666
eg7001 <110489363+eg7001@users.noreply.github.com>
2025-03-10 22:37:08 +0100

    pak i shtova po se testova hiq

--- src/main/java/com/pdox/springsectest/service/JWTService.java
+++ src/main/java/com/pdox/springsectest/service/JWTService.java
@@ -1,5 +1,6 @@
 package com.pdox.springsectest.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -14,7 +15,7 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.function.Function;
 @Service
 public class JWTService {
     private String secretKey = "";
@@ -41,16 +42,38 @@ public class JWTService {
                 .compact();
     }
 
-    private Key getKey() {
+    private SecretKey getKey() {
         byte[]keyByte = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyByte);
     }
 
     public String extractUserName(String token) {
-        return "";
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims,T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     public boolean validateToken(String token, UserDetails userDetails) {
-        return true;
+        final String userName = extractUserName(token);
+        return(userName.equals(userDetails.getUsername())&& !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+       return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 }
